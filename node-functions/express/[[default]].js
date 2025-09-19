e1aead206b4214c7a393037ac86b311b944c90cf
Carol <lxw@cnkj.site>
2025-09-19 10:35:24 +0800

    无法在eo pages上部署

--- node-functions/express/[[default]].js
+++ node-functions/express/[[default]].js
@@ -13,7 +13,7 @@ console.log('Current working directory:', process.cwd());
 console.log('Is EdgeOne environment:', isEdgeOne);
 
 // 在EdgeOne环境中，直接使用绝对路径
-const imageDirPath = 'assets/images';
+const imageDirPath = '../../dist/assets/images';
 console.log('Image directory path:', imageDirPath);
 
 // 检查图片目录是否存在
@@ -57,10 +57,8 @@ app.use(cors({
 // 完全移除JSON解析器，使用原始请求处理
 
 // 生产环境：优先服务前端构建文件
-app.use(express.static('./'));
-//app.use(express.static('../../dist'));
-//app.use('/assets', express.static('../../public/assets'));
-app.use('/assets', express.static('./assets'));
+app.use(express.static('../../dist'));
+app.use('/assets', express.static('../../public/assets'));
 
 // 安全头
 app.use((req, res, next) => {
@@ -390,8 +388,7 @@ printProjectStructure();
 
 // 生产环境：所有其他路由都返回 index.html（SPA 路由支持）
 app.get('*', (req, res) => {
-//  res.sendFile('../../dist/index.html');
-  res.sendFile('index.html');
+  res.sendFile('../../dist/index.html');
 });
 
 export default app;
